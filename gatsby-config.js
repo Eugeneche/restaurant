5d578fe933d1d49e7f5e434748845dad5eda4c6f
Eugeneche <eugene.bizmail@gmail.com>
2023-08-06 13:27:14 +0200

    og image 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,7 @@ module.exports = {
     title: `Restaurant Nebula Eats`,
     description: `Come and experience our warm hospitality, delicious cuisine, and inviting atmosphere. We look forward to welcoming you to our restaurant.`,
     author: `@websolutionsforyou.com`,
-    siteUrl: `https://thunderous-manatee-291bcd.netlify.app/`,
-    devUrl: `http://localhost:8000`,
+    siteUrl: `https://thunderous-manatee-291bcd.netlify.app`,
   },
   plugins: [
     `gatsby-plugin-image`,
